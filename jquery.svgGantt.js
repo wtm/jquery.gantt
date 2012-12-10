2e2312b3f1874e71d8fd6a48818f39fff519ff4e
Scott W. Elwood <McPants32@gmail.com>
2012-12-10 11:52:11 -0800

    Changed objects to projects for better naming conventions

--- jquery.svgGantt.js
+++ jquery.svgGantt.js
@@ -17,11 +17,11 @@
         }
       };
 
-  function svgGantt( element, objects, options ) {
+  function svgGantt( element, projects, options ) {
     var sg = this;
     sg.container = $(element);
     sg.options = $.extend( {}, defaults, options );
-    sg.objects = objects;
+    sg.projects = projects;
     sg.init();
   }
 
@@ -31,7 +31,7 @@
       var sg = this;
 
       // One time calculations
-      sg.parseObjects(); // Modify initial object data
+      sg.parseProjects(); // Modify initial project data
       sg.createUI(); // Create the UI elements (labels, content, grid)
       sg.render(); // Render the actual content
     },
@@ -42,13 +42,13 @@
       console.time("render time")
       sg.clearUI(); // Clear any data from last render
       sg.setTimeframe(); // Determine where in time we are
-      sg.setActiveObjects(); // Only get the objects in the current timeframe
+      sg.setActiveProjects(); // Only get the projects in the current timeframe
 
       // Physical Application
       sg.drawGrid(); // Draw the grid background
       sg.setPosition(); // Determine the current visual position in time
       sg.drawLabels(); // Draw the date labels
-      sg.createElements(); // Loop through the objects and create elements
+      sg.createElements(); // Loop through the projects and create elements
       sg.arrangeElements(); // Arrange those elements by start date
 
       // Interactions
@@ -57,23 +57,23 @@
       console.timeEnd("render time")
     },
 
-    parseObjects: function() {
+    parseProjects: function() {
       var sg = this,
-          objects = sg.objects;
+          projects = sg.projects;
 
-      // Go over each object and modify the data.
-      for(i=0;i<objects.length;i++) {
-        var object = objects[i];
+      // Go over each project and modify the data.
+      for(i=0;i<projects.length;i++) {
+        var project = projects[i];
 
         // Convert to Unix time.
-        if(isNaN(object.startDate)) {
-          object.startDate = moment(object.startDate).unix();
-          object.endDate = moment(object.endDate).unix();
+        if(isNaN(project.startDate)) {
+          project.startDate = moment(project.startDate).unix();
+          project.endDate = moment(project.endDate).unix();
         }
       }
 
       // Sort them by their start time / ID
-      objects.sort(function(a,b) {
+      projects.sort(function(a,b) {
         isBefore = a.startDate - b.startDate;
         if(!isBefore) { isBefore = a.id - b.id }
         return isBefore
@@ -124,25 +124,25 @@
       sg.endMoment = moment(sg.startMoment).add("days", sg.daysInGrid);
     },
 
-    setActiveObjects: function() {
+    setActiveProjects: function() {
       var sg = this, options = sg.options,
           view = options.views[options.view],
-          objects = sg.objects;
+          projects = sg.projects;
 
-      // Set the live objects to only be those that are in view
-      sg.activeObjects = [];
+      // Set the live projects to only be those that are in view
+      sg.activeProjects = [];
       var timelinePadding = (view.timelinePadding * 24*60*60),
           timelineStart = sg.startMoment.unix() - timelinePadding,
           timelineEnd = moment(sg.startMoment).add("days", sg.daysInGrid).unix() + timelinePadding;
 
-      // Determine if the object falls in between the current time frame
-      for(i=0;i<objects.length;i++) {
-        var object = objects[i],
-            isBetweenStart = sg.isBetween(timelineStart,object.startDate,timelineEnd),
-            isBetweenEnd = sg.isBetween(timelineStart,object.endDate,timelineEnd);
+      // Determine if the project falls in between the current time frame
+      for(i=0;i<projects.length;i++) {
+        var project = projects[i],
+            isBetweenStart = sg.isBetween(timelineStart,project.startDate,timelineEnd),
+            isBetweenEnd = sg.isBetween(timelineStart,project.endDate,timelineEnd);
         if(isBetweenStart || isBetweenEnd) {
-          object.ganttRow = 0;
-          sg.activeObjects.push(object);
+          project.ganttRow = 0;
+          sg.activeProjects.push(project);
         }
       }
     },
@@ -238,17 +238,17 @@
           mode = options.modes[options.mode],
           view = options.views[options.view],
           gridX = view.gridX,
-          objects = sg.activeObjects,
+          projects = sg.activeProjects,
           elements = []
           el_i = 0,
           el_height = view.gridY * mode.scale;
 
-      for(i=0;i<objects.length;i++) {
-        var object = objects[i],
+      for(i=0;i<projects.length;i++) {
+        var project = projects[i],
 
-            // Determine the object date
-            startDate = moment.unix(object.startDate),
-            endDate = moment.unix(object.endDate),
+            // Determine the project date
+            startDate = moment.unix(project.startDate),
+            endDate = moment.unix(project.endDate),
             daysBetween = endDate.diff(startDate, "days") + 1,
             daysSinceStart = startDate.diff(sg.startMoment, "days"),
 
@@ -256,36 +256,36 @@
             width = daysBetween * gridX,
             left = daysSinceStart * gridX;
 
-        // Physical object element
-        elements[el_i++] = '<div class="sg-object" style="'+
+        // Physical project element
+        elements[el_i++] = '<div class="sg-project" style="'+
                   'height:'+el_height+'px;'+
                   'left:'+left+'px;'+
                   'top: -30px;'+
                   'width:'+width+'px;">';
 
         elements[el_i++] = '<div class="sg-data" style="'+
-                    'background:'+object.color+';'+
+                    'background:'+project.color+';'+
                     'height:'+el_height+'px;'+
                     'width:'+width+'px;">';
 
-        // If the object content is visible
+        // If the project content is visible
         if(mode.showContent) {
           // The image icon
           elements[el_i++] = '<img class="sg-icon" '+
-                      'src="'+object.iconURL+'" style="'+
+                      'src="'+project.iconURL+'" style="'+
                       'height: '+el_height+';'+
                       'width: '+el_height+';" />';
 
           // The name
           elements[el_i++] = '<div class="sg-name" style="'+
                       'width: '+(width - el_height - 8)+'px;">'+
-                      object.name + "</div>";
+                      project.name + "</div>";
 
           // The moments
           elements[el_i++] = '<div class="sg-moments">';
 
-          for(j=0;j<object.moments.length;j++) {
-            var objMoment = object.moments[j],
+          for(j=0;j<project.moments.length;j++) {
+            var objMoment = project.moments[j],
                 left = moment(startDate).diff(objMoment.date, "days") * gridX;
             elements[el_i++] = '<div class="sg-moment" style="'+
                         'left: '+left+'px;"></div>'
@@ -295,7 +295,7 @@
         } else {
           elements[el_i++] = "</div>"; // Close sg-data
         }
-        elements[el_i++] = "</div>"; // Close sg-object
+        elements[el_i++] = "</div>"; // Close sg-project
       }
       sg.content.append(elements.join(''));
     },
@@ -303,39 +303,39 @@
     arrangeElements: function(animated) {
       var sg = this, options = sg.options,
           mode = options.modes[options.mode],
-          $objects = sg.content.children(),
+          $projects = sg.content.children(),
           gridY = options.views[options.view].gridY,
           paddingY = gridY * mode.paddingY,
           paddingX = mode.paddingX * (24*60*60),
-          objectHeight = gridY * mode.scale,
-          objects = sg.activeObjects;
+          projectHeight = gridY * mode.scale,
+          projects = sg.activeProjects;
 
-      // Loop over each object
-      for(var i=0;i<objects.length;i++) {
-        // Get the date data for the current object
-        var selected = objects[i],
+      // Loop over each project
+      for(var i=0;i<projects.length;i++) {
+        // Get the date data for the current project
+        var selected = projects[i],
             selectedStart = selected.startDate - paddingX,
             selectedEnd = selected.endDate + paddingX,
-            $selected = $($objects[i]),
+            $selected = $($projects[i]),
             row = 0,
             usedRows = [];
 
         // TODO: Clean this up to prevent unecessary calculations
-        // Loop over every object before this one
+        // Loop over every project before this one
         for(var j=0;j<i;j++) {
-          // Determine if this object is within the range of the
+          // Determine if this project is within the range of the
           // currently selected one.
-          var object = objects[j],
-              objectStart = object.startDate,
-              objectEnd = object.endDate,
-              betweenObjectStart = sg.isBetween(objectStart, selectedStart, objectEnd),
-              betweenObjectEnd = sg.isBetween(objectStart, selectedEnd, objectEnd),
-              betweenSelectedStart = sg.isBetween(selectedStart, objectStart, selectedEnd),
-              betweenSelectedEnd = sg.isBetween(selectedStart, objectEnd, selectedEnd);
+          var project = projects[j],
+              projectStart = project.startDate,
+              projectEnd = project.endDate,
+              betweenProjectStart = sg.isBetween(projectStart, selectedStart, projectEnd),
+              betweenProjectEnd = sg.isBetween(projectStart, selectedEnd, projectEnd),
+              betweenSelectedStart = sg.isBetween(selectedStart, projectStart, selectedEnd),
+              betweenSelectedEnd = sg.isBetween(selectedStart, projectEnd, selectedEnd);
 
           // If it is, then we must move it down a row to compensate
-          if(betweenObjectStart || betweenObjectEnd || betweenSelectedStart || betweenSelectedEnd) {
-            usedRows.push(object.ganttRow);
+          if(betweenProjectStart || betweenProjectEnd || betweenSelectedStart || betweenSelectedEnd) {
+            usedRows.push(project.ganttRow);
           }
         }
 
@@ -350,7 +350,7 @@
 
         // Set the vertical offset
         attributes = {
-          top: paddingY + (row * (objectHeight + paddingY)),
+          top: paddingY + (row * (projectHeight + paddingY)),
           zIndex: 5000 - (row * 10)
         }
         if(animated) {
@@ -446,10 +446,10 @@
 
   // A really lightweight plugin wrapper around the constructor,
   // preventing against multiple instantiations
-  $.fn[pluginName] = function ( objects, options ) {
+  $.fn[pluginName] = function ( projects, options ) {
     return this.each(function () {
       if (!$.data(this, "plugin_" + pluginName)) {
-        $.data(this, "plugin_" + pluginName, new svgGantt( this, objects, options ));
+        $.data(this, "plugin_" + pluginName, new svgGantt( this, projects, options ));
       }
     });
   };
