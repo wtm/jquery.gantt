8ad77b1299ecc1c4d62ca2422cf2c477ea397c48
Scott W. Elwood <McPants32@gmail.com>
2012-12-12 15:08:07 -0800

    Updated number of elements

--- demo/demo.js
+++ demo/demo.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  var storyCount = 5400,
+  var storyCount = 200,
       colors = ["red", "green", "brown", "purple", "pink", "orange"],
       months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
       stories = [];
