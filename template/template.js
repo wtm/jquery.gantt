9b735499da2e3933db1780c61e289493dfdd7616
Scott W. Elwood <McPants32@gmail.com>
2012-12-19 10:55:53 -0800

    Updated demo

--- template/template.js
+++ template/template.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  $('.filter button').on("click", function() {
+  $('.filters .btn-group.toggle button').on("click", function() {
     $(this).siblings(".active").removeClass("active");
     $(this).addClass("active");
   })
